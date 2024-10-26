3018c421a7bc896abc19f0160ddc461171118161
Giovanni Lucarelli <gio.lucarelli99@gmail.com>
2024-10-26 19:36:22 +0200

    Revert "Implemented concrete methods"

--- main.cpp
+++ main.cpp
@@ -1,114 +1,52 @@
 #include <iostream>
 #include <vector>
-#include <stdexcept>
-#include <iomanip>
 
 class SparseMatrix {
+
 public:
-    virtual unsigned int get_columns() = 0;
-    virtual unsigned int get_rows() = 0;
-    virtual unsigned int get_nnz() = 0;
-    virtual double operator() (unsigned int row_idx, unsigned int col_idx) const = 0;  //reading on const
-    virtual double& operator() (unsigned int row_idx, unsigned int col_idx) = 0;        //reading on non const
+    std::vector<double> values;
+    //virtual unsigned int get_columns() = 0;
+    //virtual unsigned int get_rows() = 0;
+    /*virtual unsigned int get_nnz() = 0;*/
+    virtual void fromDense(std::vector<std::vector<double>>& matrix) = 0;
+    /*virtual double& operator() (unsigned int row_idx, unsigned int col_idx) const = 0;  //reading
+    virtual double& operator() (unsigned int row_idx, unsigned int col_idx) = 0;        //writing
+    virtual SparseMatrix& operator=(double new_value) = 0; //assigning
     //matrix product
-    virtual std::vector<double> operator*(const std::vector<double>& vec) const = 0;
-    virtual void print() const = 0;
-    //TODO print full matrix (w zeros)
-    //base destructor
+    virtual std::vector<double>& operator*(std::vector<double> vec) const = 0;
+    virtual void print() const = 0;*/
     virtual ~SparseMatrix() = default;
 };
 
 class SparseMatrixCOO : public SparseMatrix {
 private:
-    unsigned int numRows;
-    unsigned int numCols;
-    std::vector<double> values;
     std::vector<unsigned int> rows;
     std::vector<unsigned int> columns;
-    static const double zero;
 public:
-    // COO constructor
-    explicit SparseMatrixCOO(const std::vector<std::vector<double>>& matrix) {
-        numRows = matrix.size();
-        numCols = matrix[0].size();
+    void fromDense(std::vector<std::vector<double>>& matrix) override {
         for (unsigned int i = 0; i < matrix.size(); i++) {
             for (unsigned int j = 0; j < matrix[i].size(); j++) {
                 if (matrix[i][j] != 0) {
                     values.push_back(matrix[i][j]);
-                    rows.push_back(i);
-                    columns.push_back(j);
+                    rows.push_back(1);
+                    columns.push_back(2);
                 }
             }
         }
     }
-    unsigned int get_columns() override {
-        return numCols;
-    }
-    unsigned int get_rows() override {
-        return numRows;
-    }
-    unsigned int get_nnz() override {
-        return values.size();
-    }
-    double operator() (const unsigned int row_idx, const unsigned int col_idx) const override {
-        if (row_idx > numRows || col_idx > numCols || row_idx <= 0 || col_idx <= 0) {
-            throw std::invalid_argument("row index or column index is out of range");
-        }
-        for (unsigned int i = 0; i < rows.size(); i++) {
-            if (rows[i]==(row_idx-1) and columns[i] == (col_idx-1)) {
-                return values[i];
-            }
-        }
-        return 0.0;
-    }
-    double& operator() (const unsigned int row_idx, const unsigned int col_idx) override {
-        if (row_idx > numRows || col_idx > numCols || row_idx <= 0 || col_idx <= 0) {
-            throw std::invalid_argument("row index or column index is out of range");
-        }
-        for (unsigned int i = 0; i < rows.size(); i++) {
-            if (rows[i]==(row_idx-1) and columns[i] == (col_idx-1)) {
-                return values[i];
-            }
-        }
-
-        std::cout << "Warning: Entry at (" << row_idx << ", " << col_idx << ") not allocated. Allocating now." << std::endl;
-        rows.push_back(row_idx-1);
-        columns.push_back(col_idx-1);
-        values.push_back(0.0);
-        //Return a reference to the newly allocated entry
-        return values.back();
-    }
-    std::vector<double> operator*(const std::vector<double>& vec) const override {
-        if (vec.size() != numCols) {
-            throw std::invalid_argument("Vector size must match the number of columns in the matrix.");
-        }
-        std::vector<double> result(numRows, 0.0); // Initialize result vector with zeros
-        for (unsigned int i = 0; i <= values.size(); ++i) {
-            result[rows[i]] += values[i] * vec[columns[i]];
-        }
-        return result;
-    }
-    void print() const override {
-        std::cout << "Sparse Matrix (COO format):" << std::endl;
-        std::cout << "Row\tColumn\tValue" << std::endl;
-        for (size_t i = 0; i < values.size(); ++i) {
-            std::cout << rows[i] << "\t" << columns[i] << "\t" << std::setw(8) << values[i] << std::endl;
-        }
-
-    }
 };
 
 int main() {
-    const std::vector<std::vector<double>> matrix ={
+    std::vector<std::vector<double>> matrix ={
             {0, 0, 3},
             {0, 0, 0},
             {1, 0, 2}
         };
 
-    SparseMatrixCOO sparse1(matrix);
+    SparseMatrixCOO sparse1;
 
-    sparse1(1,1) = 7.0;
-    sparse1.print();
+    sparse1.fromDense(matrix);
 
+    std::cout << sparse1.values[2] << std::endl;
     return 0;
 }
